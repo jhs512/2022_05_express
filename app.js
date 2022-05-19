9839988b2a9d3d96cc3e63f72e074d78b250d27a
jhs512 <jangka512@gmail.com>
2022-05-19 17:37:13 +0900

    세팅

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/aa", (req, res) => {
+  res.send("Hello World!, aa");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
